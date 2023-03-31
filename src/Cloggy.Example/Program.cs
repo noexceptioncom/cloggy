a4839bd1aa7ed1a1a0c28def02e6d6cfaae0c310
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-31 11:33:08 +0200

    feat: Add more functionality to the demo

--- src/Cloggy.Example/Program.cs
+++ src/Cloggy.Example/Program.cs
@@ -3,4 +3,10 @@
 using Cloggy;
 var logger = new Logger(new LoggerConfig(new SystemConsole(), new SystemDateProvider(), true));
 
-logger.Log("Hola mundo!");
+logger.Log("Log con fecha");
+logger.LogInformation("Log information");
+logger.LogWarning("Log warning");
+
+
+var loggerWithoutDate = new Logger(new LoggerConfig(new SystemConsole(), new SystemDateProvider(), false));
+loggerWithoutDate.Log("Log simple");
