481978f2e361fe1911b39e360b6de66d681e72c1
Pedro Pardal <ppardalj@gmail.com>
2023-04-11 17:14:12 +0200

    refactor: move class to their own file

---
+++ src/Cloggy/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace Cloggy;
+
+public class Message
+{
+    public Message(string? message, LogLevel logLevel, DateTime timestamp, Category category)
+    {
+        Text = message;
+        LogLevel = logLevel;
+        Timestamp = timestamp;
+        Category = category;
+    }
+
+    public string? Text { get; private set; }
+    public LogLevel LogLevel { get; private set; }
+    public DateTime Timestamp { get; private set; }
+    public Category Category { get; private set; }
+}
--- src/Cloggy/MessageFormatter.cs
+++ src/Cloggy/MessageFormatter.cs
@@ -1,21 +1,5 @@
 ﻿namespace Cloggy;
 
-public class Message
-{
-    public Message(string? message, LogLevel logLevel, DateTime timestamp, Category category)
-    {
-        Text = message;
-        LogLevel = logLevel;
-        Timestamp = timestamp;
-        Category = category;
-    }
-
-    public string? Text { get; private set; }
-    public LogLevel LogLevel { get; private set; }
-    public DateTime Timestamp { get; private set; }
-    public Category Category { get; private set; }
-}
-
 public class MessageFormatter
 {
     private readonly bool _asJson;
