525f508ffdadf332d83b9bcc9e0e985e915f51d3
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-12 16:34:16 +0200

    test: AddOtherMessage

--- tests/Cloggy.Tests/MemoryShould.cs
+++ tests/Cloggy.Tests/MemoryShould.cs
@@ -17,5 +17,19 @@ namespace Cloggy.Tests
             
             result.Should().Be(message);
         }
+
+        [Test]
+        public void AddOtherMessage()
+        {
+            var message = new Message("other message", LogLevel.WRN, DateTime.Parse("2023-04-30T21:30:06"),
+                new Category("otherCategory"));
+
+            var memory = new Memory();
+
+            memory.AddMessage(message);
+            var result = memory.ReadAll().First();
+            
+            result.Should().Be(message);
+        }
     }
 }
