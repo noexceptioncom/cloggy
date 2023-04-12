e61c150ec82c5d425df763b06bca9de95be2f828
cvigueras <carlosj.vigueras@gmail.com>
2023-04-12 17:24:08 +0200

    Test:     public void FormatAnotherLogLevelAsXML

--- tests/Cloggy.Tests/MessageFormatterShould.cs
+++ tests/Cloggy.Tests/MessageFormatterShould.cs
@@ -146,4 +146,15 @@ public class MessageFormatterShould
 
         result.Should().Be(expectedResult);
     }
+
+    [Test]
+    public void FormatAnotherLogLevelAsXML()
+    {
+        var result = new MessageFormatter(false,true).FormatMessage(new Message("otro mensaje", LogLevel.WRN,
+            DateTime.Parse("2023-03-04T21:30:06"), new Category("ACategory")));
+
+        var expectedResult = """<log timestamp="2023-03-04T21:30:06" loglevel="WRN" category="ACategory" message="otro mensaje"></log>""";
+
+        result.Should().Be(expectedResult);
+    }
 }
