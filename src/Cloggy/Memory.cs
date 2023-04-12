16d8eeb595d28d816a3697b65a71d2766a42c006
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-12 16:37:39 +0200

    feat: add field list to memory

--- src/Cloggy/Memory.cs
+++ src/Cloggy/Memory.cs
@@ -2,17 +2,20 @@ namespace Cloggy;
 
 public class Memory
 {
+    private readonly IList<Message> _messages;
+
+    public Memory()
+    {
+        _messages = new List<Message>();
+    }
+
     public void AddMessage(Message message)
     {
-        
+        _messages.Add(message);
     }
 
     public IEnumerable<Message> ReadAll()
     {
-        return new List<Message>()
-        {
-            new Message("message", LogLevel.INF, DateTime.Parse("2023-03-30T21:30:06"),
-                new Category("category"))
-        };
+        return _messages;
     }
 }
