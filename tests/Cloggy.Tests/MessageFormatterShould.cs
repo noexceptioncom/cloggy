f1a2c84b26dc93afc48ec331ad5c0b8b3d2d3a4b
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-04-12 16:04:30 +0100

    refactor: FormatAMessage TestCases

--- tests/Cloggy.Tests/MessageFormatterShould.cs
+++ tests/Cloggy.Tests/MessageFormatterShould.cs
@@ -25,33 +25,6 @@ public class MessageFormatterShould
         message.Should().Be(expectedMessage);
     }
 
-    [Test]
-    public void FormatEmptyMessage()
-    {
-        var message = _plainTextMessageFormatter.FormatMessage(new Message(string.Empty, LogLevel.INF,
-            DateTime.Parse("2023-03-30T21:30:06"), new Category("category")));
-
-        message.Should().Be("[2023-03-30T21:30:06 INF (category)] ");
-    }
-
-    [Test]
-    public void FormatOneWordMessage()
-    {
-        var message = _plainTextMessageFormatter.FormatMessage(new Message("juanvi", LogLevel.INF,
-            DateTime.Parse("2023-03-30T21:30:06"), new Category("category")));
-
-        message.Should().Be("[2023-03-30T21:30:06 INF (category)] juanvi");
-    }
-
-    [Test]
-    public void FormatNullMessage()
-    {
-        var message = _plainTextMessageFormatter.FormatMessage(new Message(null, LogLevel.INF,
-            DateTime.Parse("2023-03-30T21:30:06"), new Category("category")));
-
-        message.Should().Be("[2023-03-30T21:30:06 INF (category)] ");
-    }
-
     [TestCase("ACategory", "A message")]
     [TestCase("OtherCategory", "Other message")]
     [TestCase("AnotherCategory", "Another message")]
