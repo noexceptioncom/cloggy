4f294a534c937f06d12cb9437dd30ae3d6a28da2
cvigueras <carlosj.vigueras@gmail.com>
2023-04-12 17:37:48 +0200

    Add: addedd enum class Format.

---
+++ src/Cloggy/Format.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloggy
+{
+    public enum Format
+    {
+        PlainText = 0,
+        Json,
+        Xml,
+    }
+}
