c469f6e1bc4c9556a67627a258d02877d2434cd5
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-13 14:24:40 +0200

    refactor: Extract format strategy class

---
+++ src/Cloggy/FormatStrategy.cs
@@ -0,0 +1,44 @@
+﻿namespace Cloggy;
+
+public class FormatStrategy
+{
+    private MessageFormatter _messageFormatter;
+
+    public FormatStrategy(MessageFormatter messageFormatter)
+    {
+        _messageFormatter = messageFormatter;
+    }
+
+    public string FormatMessageByStrategy(Message message)
+    {
+        if (_messageFormatter.Format == Format.Json)
+        {
+            return FormatMessageAsJson(message);
+        }
+
+        if (_messageFormatter.Format == Format.Xml)
+        {
+            return FormatMessageAsXML(message);
+        }
+
+        return FormatMessageAsPlainText(message);
+    }
+
+    private static string FormatMessageAsXML(Message message)
+    {
+        return
+            $$"""<log timestamp="{{message.Timestamp}}" loglevel="{{message.LogLevel}}" category="{{message.Category}}" message="{{message.Text}}"></log>""";
+    }
+
+    private string FormatMessageAsPlainText(Message message)
+    {
+        var header = string.Join(' ', message.Timestamp, message.LogLevel, $"({message.Category})").Trim();
+        return $"[{header}] {message.Text}";
+    }
+
+    private string FormatMessageAsJson(Message message)
+    {
+        return
+            $$"""{"timestamp":"{{message.Timestamp}}","logLevel":"{{message.LogLevel}}","category":"{{message.Category}}","message":"{{message.Text}}"}""";
+    }
+}
--- src/Cloggy/MessageFormatter.cs
+++ src/Cloggy/MessageFormatter.cs
@@ -3,42 +3,21 @@
 public class MessageFormatter
 {
     private readonly Format _format;
+    private readonly FormatStrategy _formatStrategy;
 
     public MessageFormatter(Format format)
     {
         _format = format;
+        _formatStrategy = new FormatStrategy(this);
+    }
+
+    public Format Format
+    {
+        get { return _format; }
     }
 
     public string FormatMessage(Message message)
     {
-        if (_format == Format.Json)
-        {
-            return FormatMessageAsJson(message);
-        }
-
-        if (_format == Format.Xml)
-        {
-            return FormatMessageAsXML(message);
-        }
-
-        return FormatMessageAsPlainText(message);
-    }
-
-    private static string FormatMessageAsXML(Message message)
-    {
-        return
-            $$"""<log timestamp="{{message.Timestamp}}" loglevel="{{message.LogLevel}}" category="{{message.Category}}" message="{{message.Text}}"></log>""";
-    }
-
-    private string FormatMessageAsPlainText(Message message)
-    {
-        var header = string.Join(' ', message.Timestamp, message.LogLevel, $"({message.Category})").Trim();
-        return $"[{header}] {message.Text}";
-    }
-
-    private string FormatMessageAsJson(Message message)
-    {
-        return
-            $$"""{"timestamp":"{{message.Timestamp}}","logLevel":"{{message.LogLevel}}","category":"{{message.Category}}","message":"{{message.Text}}"}""";
+        return _formatStrategy.FormatMessageByStrategy(message);
     }
 }
