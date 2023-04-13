cf9847b33f8f6337de7fc5f6f50c545ad9b1a64e
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-13 16:52:38 +0200

    refactors: inline methods

--- src/Cloggy.Example/Program.cs
+++ src/Cloggy.Example/Program.cs
@@ -1,42 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Cloggy;
+
 var category = "Category1";
-var logger = LoggerFactory.CreatePlainTextLoggerToConsole(category);
+var logger = new LoggerBuilder(category).ToConsole().Build();
 
 logger.LogInformation("Log con fecha");
 logger.LogError("Error Log information");
 logger.LogWarning("Log warning");
 
-var jsonLogger = LoggerFactory.CreateJsonLoggerToConsole(category);
+var jsonLogger = new LoggerBuilder(category).WithJsonFormat().ToConsole().Build();
 jsonLogger.LogInformation("Log info");
 jsonLogger.LogError("Log error");
 jsonLogger.LogWarning("Log warning");
 
-var plainTextLoggerToFile = LoggerFactory.CreatePlainTextLoggerToFile(category, @".\plainTextLogger.txt");
+var plainTextLoggerToFile = new LoggerBuilder(category).ToFile(@".\plainTextLogger.txt").Build();
 plainTextLoggerToFile.LogInformation("Log Info to File");
 plainTextLoggerToFile.LogInformation("Log Info to File second line");
 
-var jsonLoggerToFile= LoggerFactory.CreateJsonLoggerToFile(category, @".\jsonLogger.json");
+var jsonLoggerToFile= new LoggerBuilder(category).WithJsonFormat().ToFile(@".\jsonLogger.json").Build();
 jsonLoggerToFile.LogInformation("Log Json Info to File");
 jsonLoggerToFile.LogInformation("Log Json Info to File second line");
 
-var plainTextLoggerToFileAndConsole = LoggerFactory.CreatePlainTextLoggerToFileAndConsole(category, @".\plainTextLogger.txt");
+var plainTextLoggerToFileAndConsole = new LoggerBuilder(category).ToFile(@".\plainTextLogger.txt").ToConsole().Build();
 plainTextLoggerToFileAndConsole.LogInformation("Log Info to File and Console");
 plainTextLoggerToFileAndConsole.LogInformation("Log Info to File and Console second line");
 
-var jsonLoggerToFileAndConsole = LoggerFactory.CreateJsonLoggerToFileAndConsole(category, @".\jsonLogger.json");
+var jsonLoggerToFileAndConsole = new LoggerBuilder(category).WithJsonFormat().ToFile(@".\jsonLogger.json").ToConsole().Build();
 jsonLoggerToFileAndConsole.LogInformation("Log Json Info to File and Console");
 jsonLoggerToFileAndConsole.LogInformation("Log Json Info to File and Console second line");
 
-var plainTextLoggerInMemory = LoggerFactory.CreateLoggerInMemory(category);
+var plainTextLoggerInMemory = new LoggerBuilder(category).ToMemory().Build();
 plainTextLoggerInMemory.LogInformation("Log Info in Memory");
 plainTextLoggerInMemory.LogInformation("Log Info in Memory second Line");
 
-var xmlLoggerToFile = LoggerFactory.CreateXmlLoggerToFile(category, @".\xmlLogger.xml");
+var xmlLoggerToFile = new LoggerBuilder(category).WithXmlFormat().ToFile(@".\xmlLogger.xml").Build();
 xmlLoggerToFile.LogInformation("Log XML Info to File");
 xmlLoggerToFile.LogInformation("Log XML Info to File second line");
 
-var xmlLoggerToFileAndConsole = LoggerFactory.CreateXmlLoggerToFileAndConsole(category, @".\xmlLogger.xml");
+var xmlLoggerToFileAndConsole = new LoggerBuilder(category).WithXmlFormat().ToFile(@".\xmlLogger.xml").ToConsole().Build();
 xmlLoggerToFileAndConsole.LogInformation("Log XML Info to File and Console");
 xmlLoggerToFileAndConsole.LogInformation("Log XML Info to File second line and Console");
